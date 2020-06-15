7a2d3e957b8a19daecd5fe764af7aefc121fb091
Ian <ian.christopher.mason@gmail.com>
2020-06-14 19:25:53 -0500

    add debug, random key

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 import sys
+import os
 
 from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
     DEBUG=True,
